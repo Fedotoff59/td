fc366e6524d74e61379d0fc436e69eb424ea2cc9
Fedotoff MBP <denis.fedotoff@gmail.com>
2016-09-22 21:36:26 +0500

    Создана страница для размещение формы

---
+++ forms/member/index.php
@@ -0,0 +1,14 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$GLOBALS["arrFilterMainTheme"] = array("PROPERTY_MAIN_VALUE" => 1);
+$GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
+?>
+    <div class="block b-form">
+        <div>
+            <h1>Заявка - краевой слет</h1>
+        </div>
+        <div class="member-form-block">
+             Форма
+        </div>
+    </div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
