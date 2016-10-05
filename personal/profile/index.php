bfa8f826a34433139bc640ce00c49b169520f02b
Fedotoff MBP <denis.fedotoff@gmail.com>
2016-10-05 12:36:41 +0500

    Настроен базовый функционал профиля пользователя

--- personal/profile/index.php
+++ personal/profile/index.php
@@ -1,8 +1,17 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Настройки пользователя");
-?><?$APPLICATION->IncludeComponent("bitrix:main.profile", ".default", Array(
-	"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
-	),
-	false
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+<div class="block">
+	<div>
+		<h1>Личный кабинет пользователя</h1>
+	</div>
+	<div class="profile">
+		<?$APPLICATION->IncludeComponent("bitrix:main.profile", "td", Array(
+			"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
+			),
+			false
+		);?>
+	</div>
+</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
