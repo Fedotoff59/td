9ff9bfce894fa2f8cc95118f25283302989b87a8
Fedotoff MBP <denis.fedotoff@gmail.com>
2016-10-13 19:02:59 +0500

    Настроена верстка условий конкурсов

---
+++ personal/interactive/competitions/photo/index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Online-фотоконкурс «Мой любимый кинокадр» - Детско-молодёжная общероссийская общественная организация «Тетрадка Дружбы»");
+define("NEED_AUTH", true);
+?>
+    <div class="block b-profile">
+        <div>
+            <h1>Online-фотоконкурс «Мой любимый кинокадр»</h1>
+        </div>
+
+    </div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ personal/interactive/competitions/video/index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Конкурс видеороликов «Самый-самый детства мир» - Детско-молодёжная общероссийская общественная организация «Тетрадка Дружбы»");
+define("NEED_AUTH", true);
+?>
+    <div class="block b-profile">
+        <div>
+            <h1>Конкурс видеороликов «Самый-самый детства мир»</h1>
+        </div>
+
+    </div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
