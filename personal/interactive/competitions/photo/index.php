5a7719584d2ff27032c3f9890f04cfe395873bd7
Fedotoff MBP <denis.fedotoff@gmail.com>
2016-10-14 14:14:41 +0500

    Размещены формы в личном кабинете

--- personal/interactive/competitions/photo/index.php
+++ personal/interactive/competitions/photo/index.php
@@ -7,6 +7,14 @@ define("NEED_AUTH", true);
         <div>
             <h1>Online-фотоконкурс «Мой любимый кинокадр»</h1>
         </div>
-
+        <div class="googleform-block googleform-block--h1200">
+            <div class="googleform-holder googleform-holder--h1200">
+                <div class="googleform-container">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeyF6hmo4qH-MaTQT6xKdRNoLtQ8EY2RcV38tlVFrNXkqfdFA/viewform?embedded=true" width="760" height="1400" frameborder="0" marginheight="0" marginwidth="0">Загрузка...</iframe>
+                </div>
+            </div>
+        </div>
+        <p>&nbsp;</p>
+        <p><a href="/interactive/competitions/photo/">Ознакомиться с условиями конкурса</a></p>
     </div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- personal/interactive/competitions/video/index.php
+++ personal/interactive/competitions/video/index.php
@@ -7,6 +7,14 @@ define("NEED_AUTH", true);
         <div>
             <h1>Конкурс видеороликов «Самый-самый детства мир»</h1>
         </div>
-
+        <div class="googleform-block googleform-block--h1600">
+            <div class="googleform-holder googleform-holder--h1600">
+                <div class="googleform-container">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf1hRoIk1u60c_G_XSKaUHXrR-dWUVYvkdied30doFMpDyTwA/viewform?embedded=true" width="760" height="1700" frameborder="0" marginheight="0" marginwidth="0">Загрузка...</iframe>
+                </div>
+            </div>
+        </div>
+        <p>&nbsp;</p>
+        <p><a href="/interactive/competitions/video/">Ознакомиться с условиями конкурса</a></p>
     </div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
