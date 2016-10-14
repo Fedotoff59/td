9993c228dffdd87b7653bb781fcad3ead5db16ab
Fedotoff MBP <denis.fedotoff@gmail.com>
2016-10-14 14:53:03 +0500

    Добавлена общая страница конкурсов

---
+++ interactive/competitions/index.php
@@ -0,0 +1,30 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$GLOBALS["arrFilterMainTheme"] = array("PROPERTY_MAIN_VALUE" => 1);
+$GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
+$APPLICATION->SetTitle("Конкурс видеороликов «Самый-самый детства мир» - Детско-молодёжная общероссийская общественная организация «Тетрадка Дружбы»");
+?>
+        <div class="comp_page  block">
+            <img src="/img/left_border.png" alt="" class="left_border">
+            <img src="/img/right_border.png" alt="" class="right_border">
+            <div>
+                <h1>On-line конкурсы</h1>
+            </div>
+            <div class="comp">
+                <a href="/interactive/competitions/video/">
+                    <img src="/upload/interactive/competitions/comp_1.png" alt="">
+                </a>
+                <a href="/interactive/competitions/video/">
+                    <h2>Самый-самый детства мир</h2>
+                </a>
+            </div>
+            <div class="comp">
+                <a href="/interactive/competitions/photo/">
+                    <img src="/upload/interactive/competitions/comp_2.png" alt="">
+                </a>
+                <a href="/interactive/competitions/photo/">
+                    <h2>Мой любимый кинокадр</h2>
+                </a>
+            </div>
+        </div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
