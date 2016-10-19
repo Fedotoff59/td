58309d657bae436762d35980ab4076ea5fc566dd
Fedotoff MBP <denis.fedotoff@gmail.com>
2016-10-19 16:04:12 +0500

    Создана страница с формой отчета

--- personal/activities/sdg/.access.php
+++ personal/activities/sdg/.access.php
@@ -1,3 +1,4 @@
 <?
 $PERM["index.php"]["2"]="D";
+$PERM["report"]["2"]="D";
 ?>
---
+++ personal/activities/sdg/report/index.php
@@ -0,0 +1,18 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Отчет - Проект «Все дети России за мир на земле»");
+define("NEED_AUTH", true);
+?>
+    <div class="block b-profile">
+        <div>
+            <h1>Отчет «Все дети России за мир на земле»</h1>
+        </div>
+        <div class="googleform-block googleform-block--h2000">
+            <div class="googleform-holder googleform-holder--h2000">
+                <div class="googleform-container">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe_z4xoEtLF21CNsUjAaYydyfiQPFUgkUqbAmBHzF4fVdOXow/viewform?embedded=true" width="760" height="2200" frameborder="0" marginheight="0" marginwidth="0">Загрузка...</iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
