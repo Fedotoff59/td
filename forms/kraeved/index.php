600f55b73d50dc44111273a82d04cf3a2a0d86b6
Fedotoff Office <fd@m-vector.org>
2016-11-14 20:57:54 +0500

    Добавлена форма заявки

---
+++ forms/kraeved/index.php
@@ -0,0 +1,23 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$GLOBALS["arrFilterMainTheme"] = array("PROPERTY_MAIN_VALUE" => 1);
+$GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
+$APPLICATION->SetTitle("Заявка - Конкурс «Экологический краевед»");
+?>
+    <div class="block b-form">
+        <div>
+            <h1>Заявка - Конкурс «Экологический краевед»</h1>
+        </div>
+        <div class="memberform-block">
+            <a name="form1071121619" id="formAnchor1071121619"></a>
+            <script type="text/javascript" src="https://fs25.formsite.com/include/form/embedManager.js?1071121619"></script>
+            <script type="text/javascript">
+                EmbedManager.embed({
+                    key: "https://fs25.formsite.com/res/showFormEmbed?EParam=m%2FOmK8apOTDphb%2F8KCsVffLVdPv3y7IlFzpUCZwnDno%3D&1071121619",
+                    width: "100%",
+                    mobileResponsive: true
+                });
+            </script>
+        </div>
+    </div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
