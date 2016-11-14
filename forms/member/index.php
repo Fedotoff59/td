c15cfb96627a4414bcc51c3bd471a192f7558e7a
Fedotoff Office <fd@m-vector.org>
2016-11-14 21:13:46 +0500

    Hot-fix: Удаление ссылок на form-styler

--- forms/member/index.php
+++ forms/member/index.php
@@ -1,10 +1,6 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $GLOBALS["arrFilterMainTheme"] = array("PROPERTY_MAIN_VALUE" => 1);
 $GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
-use Bitrix\Main\Page\Asset;
-Asset::getInstance()->addString('<link rel="stylesheet" type="text/css" href="'.SITE_TEMPLATE_PATH.'/vendor/jquery.formstyler/jquery.formstyler.css">');
-Asset::getInstance()->addString('<script src="'.SITE_TEMPLATE_PATH.'/vendor/jquery.formstyler/jquery.formstyler.js"></script>');
-Asset::getInstance()->addString('<script src="'.SITE_TEMPLATE_PATH.'/scripts/styler.js"></script>');
 $APPLICATION->SetTitle("Заявка на принятие в члены ДМООО «Тетрадка Дружбы»");
 ?>
     <div class="block b-form">
